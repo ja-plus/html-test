58ce762a41501f8f7187d1bdcb458ecbf28b29af
JA+ <601286825@qq.com>
2022-01-10 20:44:09 +0800

    nuxt

---
+++ server/nuxt/layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <header>Nuxt</header>
+    <div class="root-content">
+    <nuxt/>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+header{
+  position: fixed;
+  top:0;
+  width: 100vw;
+  font-size: 24px;
+  line-height: 30px;
+  font-weight: bolder;
+  padding: 0 10px;
+  background-color: #fff;
+  z-index:99;
+}
+.root-content{
+  margin-top: 40px;
+}
+</style>
---
+++ server/nuxt/pages/login.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="outer">
+    <div class="inner">
+      <h2 class="header">Login</h2>
+      <div class="row username">
+        <span class="title">username: </span>
+        <input type="text">
+      </div>
+      <div class="row password">
+        <span class="title">passowrd:</span>
+        <input type="password" name="" id="">
+      </div>
+      <div class="row">
+        <button>login</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+body{
+  margin: 0;
+}
+* {
+  box-sizing: border-box;
+}
+  .outer{
+    background: #eee;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .inner{
+    border-radius: 5px;
+    box-shadow: 0 0 5px 0 #aaa;
+    background-color: #fff;
+    padding: 20px 10px;
+  }
+</style>
