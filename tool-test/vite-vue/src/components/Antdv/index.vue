c44572ace978ce84ba338a1ae6f3c88eb732af24
JA+ <601286825@qq.com>
2022-01-20 19:00:06 +0800

    test antdv

--- tool-test/vite-vue/src/components/Antdv/index.vue
+++ tool-test/vite-vue/src/components/Antdv/index.vue
@@ -2,11 +2,13 @@
   <details open>
     <summary><img src="https://next.antdv.com/assets/logo.1ef800a8.svg" alt="Ant design vue" srcset="" style="height:20px;"></summary>
     <Button />
+    <Table />
   </details>
 </template>
 
 <script setup>
   import Button from './Button.vue'
+  import Table from './Table.vue'
 </script>
 
 <style>
