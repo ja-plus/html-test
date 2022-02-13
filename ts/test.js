faf5dc61e980ee8097127ac72dcb5327ade20376
JA+ <601286825@qq.com>
2022-02-13 19:18:28 +0800

    test typescript

---
+++ ts/test.js
@@ -0,0 +1,13 @@
+var Status;
+(function (Status) {
+    Status[Status["start"] = 0] = "start";
+    Status[Status["end"] = 1] = "end";
+})(Status || (Status = {}));
+console.log(Status.start);
+console.log(0 /* start */);
+var Person = /** @class */ (function () {
+    function Person() {
+        this.name = 'tom';
+    }
+    return Person;
+}());
---
+++ ts/test.ts
@@ -0,0 +1,60 @@
+enum Status {
+  start,
+  end
+}
+const enum Status2  {
+  start,
+  end
+}
+console.log(Status.start)
+console.log(Status2.start)
+
+interface Eat{
+  eat(name:string): void
+}
+interface Run{
+  run(name:string):void
+}
+
+class Person implements Eat,Run {
+  name:string
+  public age:number
+  private readonly gender: string
+  protected school: string
+  constructor(name:string){
+    this.name = name || 'tom'
+    this.gender = 'boy'
+  }
+  eat(name: string): void {
+    throw new Error("Method not implemented.")
+  }
+  run(name: string): void {
+    throw new Error("Method not implemented.")
+  }
+  sayHi(msg:string):void{
+    console.log('hi'+ msg);
+    // this.gender = 1
+  }
+}
+class Student extends Person{
+  constructor(name:string){
+    super(name)
+    this.school = '1'
+  }
+  static create(name:string){
+    return new Person(name)
+  }
+}
+
+let person = Student.create('a') //new Person()
+
+function createNuberArray(length:number,value:number):number[]{
+  let arr = Array<number>(length).fill(value)
+  return arr
+}
+function createArray<T>(length:number,value:T):T[]{
+  let arr = Array<T>(length).fill(value)
+  return arr
+}
+
+let arr = createArray(10,1)
