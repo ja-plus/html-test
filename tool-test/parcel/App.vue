a94524859512aed12f121028fbed7092e7133934
JA+ <601286825@qq.com>
2022-02-21 20:00:28 +0800

    try parcel

---
+++ tool-test/parcel/App.vue
@@ -0,0 +1,42 @@
+<template lang='pug'>
+  div
+    h1.title hello vue
+    span {{student.name}}
+</template>
+
+<script lang='ts'>
+enum Gender{
+  man,
+  woman
+}
+interface Person{
+  name:string;
+  age:number;
+  gender: Gender
+}
+let student:Person = {
+  name: 'Tom',
+  age: 12,
+  gender: Gender.man
+}
+export default {
+  props:{},
+  data(){
+    return {
+      student
+    }
+  },
+  computed:{},
+  created(){},
+  mounted(){},
+  methods: {
+
+  },
+}
+</script>
+
+<style>
+.title{
+  color:blueviolet;
+}
+</style>
---
+++ tool-test/parcel/index.js
@@ -0,0 +1,10 @@
+import './style.css';
+import './style.less';
+console.log('hello world');
+import Vue from 'vue';
+import App from './App.vue';
+
+new Vue({
+  el: '#vue-app',
+  render: h => h(App)
+});
