60c6d0c102049dbbb89df547237bbfdbac7f1d32
JA+ <601286825@qq.com>
2022-03-10 21:53:15 +0800

    update eslint

--- ts/utils/h.ts
+++ ts/utils/h.ts
@@ -17,17 +17,13 @@ interface Attrs {
  * @param {Attrs | String | Number | HTMLElement[]} attrs 传Object为属性，传String为textContent，传数组为children
  * @param {HTMLElement[]} children
  */
-export default function h(
-  tag: string,
-  attrs?: Attrs | string | number | HTMLElement[],
-  children?: HTMLElement[]
-): HTMLElement {
+export default function h(tag: string, attrs?: Attrs | string | number | HTMLElement[], children?: HTMLElement[]): HTMLElement {
   // TODO: validate param type
   // 解析emmet 语法 (暂支持id class, 防止解析字符串影响更多性能)
   const id = tag.match(/#[\w\d_-]+/);
   // let classArr = tag.match(/(?<=\.)[\w\d_-]+/g) || []; // className // 低版本浏览器不支持零宽断言
   let classArr: string[] = tag.match(/\.[\w\d_-]+/g) || []; // className
-  classArr = classArr.map((it) => it.substring(1));
+  classArr = classArr.map(it => it.substring(1));
   tag = tag.match(/^[\w\d]+/)[0];
 
   const elem: any = document.createElement(tag);
@@ -53,10 +49,9 @@ export default function h(
 
   if (classArr) elem.classList.add(...classArr);
   if (children) {
-    children.forEach((child) => {
+    children.forEach(child => {
       if (child instanceof HTMLElement) elem.appendChild(child);
-      else if (child !== null && child !== undefined)
-        console.error(child, 'not instance of HTMLElement');
+      else if (child !== null && child !== undefined) console.error(child, 'not instance of HTMLElement');
     });
   }
 
