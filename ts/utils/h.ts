00ef9ad68a6114d785e3ce870180fbf574263201
JA+ <601286825@qq.com>
2022-03-10 21:47:57 +0800

    update h.ts

--- ts/utils/h.ts
+++ ts/utils/h.ts
@@ -1,8 +1,12 @@
+// HTMLElement属性重写
 interface Attrs {
-  [key: string]: string | { [key: string]: string } | string[];
-  style: { [key: string]: string };
-  dataset: { [key: string]: string };
-  classList: string[];
+  [key: string]: any;
+  style?: { [key: string]: string | number };
+  dataset?: { [key: string]: string | number };
+  classList?: string[];
+  onclick?(e: MouseEvent): void;
+  oncontextmenu?(e: MouseEvent): void;
+  onmouseenter?(e: MouseEvent): void;
 }
 /**
  * createElement function
@@ -10,23 +14,23 @@ interface Attrs {
  * h(tag[, attrs[,children]])
  * h(tag[, children])
  * @param {String} tag 标签名称，支持tag#id.class emmet写法，暂支持id ,class
- * @param {Attrs | String | Number | Array<HTMLElement>} attrs 传Object为属性，传String为textContent，传数组为children
- * @param {Array<HTMLElement>} children
+ * @param {Attrs | String | Number | HTMLElement[]} attrs 传Object为属性，传String为textContent，传数组为children
+ * @param {HTMLElement[]} children
  */
 export default function h(
   tag: string,
-  attrs: Attrs | string | number | HTMLElement[],
-  children: HTMLElement[]
+  attrs?: Attrs | string | number | HTMLElement[],
+  children?: HTMLElement[]
 ): HTMLElement {
   // TODO: validate param type
   // 解析emmet 语法 (暂支持id class, 防止解析字符串影响更多性能)
-  let id = tag.match(/#[\w\d_-]+/);
+  const id = tag.match(/#[\w\d_-]+/);
   // let classArr = tag.match(/(?<=\.)[\w\d_-]+/g) || []; // className // 低版本浏览器不支持零宽断言
   let classArr: string[] = tag.match(/\.[\w\d_-]+/g) || []; // className
   classArr = classArr.map((it) => it.substring(1));
   tag = tag.match(/^[\w\d]+/)[0];
 
-  let elem: any = document.createElement(tag);
+  const elem: any = document.createElement(tag);
   if (id) elem.id = id[0].substring(1);
 
   if (Array.isArray(attrs)) {
