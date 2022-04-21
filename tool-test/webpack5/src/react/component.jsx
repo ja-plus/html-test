badf65e03ad4b876ec31d65a7090f4a0bea9b535
JA+ <601286825@qq.com>
2022-04-21 17:04:44 +0800

    try react antd

--- tool-test/webpack5/src/react/component.jsx
+++ tool-test/webpack5/src/react/component.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
-HelloMessage.defaultProps = {
-  defaultProp: 'aaaa',
-};
+class Child extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h2>child</h2>
+        <div>num: {this.props.num}</div>
+        <input type="text" value={this.props.num} onChange={this.handleInputChange.bind(this)} />
+      </div>
+    );
+  }
+  handleInputChange(e) {
+    this.props.changeNum(e.target.value);
+  }
+
+  refFunc() {
+    console.log('child ref func');
+  }
+}
 export default class HelloMessage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { date: new Date() };
+    this.state = { date: new Date(), num: 0 };
+    this.childRef = React.createRef();
   }
+
   componentDidMount() {
     console.log('component Mount');
   }
@@ -20,7 +40,42 @@ export default class HelloMessage extends React.Component {
           Hello {this.props.name} {this.state.date.toLocaleDateString()}
         </h1>
         <h2>{this.props.defaultProp}</h2>
+        <button onClick={this.handleClick.bind(this)}>click{this.state.num}</button>
+
+        <ul>{this.list()}</ul>
+        <Child /* ref="child" */ ref={this.childRef} num={this.state.num} changeNum={this.handleChangeNum.bind(this)}></Child>
       </div>
     );
   }
+
+  handleClick(e) {
+    console.log('click button', e);
+    // console.log('findDomNode', this.findDOMNode());
+    // this.replaceState({ num: this.state.num++ });
+    // this.refs.child.refFunc();
+    this.childRef.current.refFunc(); // current?
+    this.setState(state => {
+      state.num++;
+      return state;
+    });
+  }
+
+  handleChangeNum(val) {
+    this.setState(state => {
+      state.num = val;
+      return state;
+    });
+  }
+
+  list() {
+    let els = [];
+    for (let i = 0; i < 3; i++) {
+      els.push(<li key={i}>{i}</li>);
+    }
+    return els;
+  }
 }
+// HelloMessage 一定在声明后使用
+HelloMessage.defaultProps = {
+  defaultProp: 'aaaa',
+};
--- tool-test/webpack5/src/reactApp.jsx
+++ tool-test/webpack5/src/reactApp.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import AntdTest from './react/antd.jsx';
 import HelloMessage from './react/component.jsx';
 class App extends React.Component {
   render() {
@@ -7,6 +8,7 @@ class App extends React.Component {
       <div style={{ color: '#333' }} className="test test2">
         <h1>hello react</h1>
         <HelloMessage name="hahaha prop" />
+        <AntdTest />
       </div>
     );
   }
