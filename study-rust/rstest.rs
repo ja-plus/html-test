4b93fffaf0d2d02bd465756fbd84b38cb3807c27
JA+ <601286825@qq.com>
2022-04-30 21:27:37 +0800

    studyRust

---
+++ study-rust/rstest.rs
@@ -0,0 +1,13 @@
+fn main() {
+  println!("Hello World!");
+  // let a = "11";
+  // println!("{}",a);
+  let mut a:i8 = 127;
+  // a = 456;
+  another_function();
+  
+}
+
+fn another_function() {
+  println!("Hello, runoob!");
+}
