8cc0ee7dafc13ba94c12fb7e256ef21aed35c55c
JA+ <601286825@qq.com>
2022-05-08 17:03:23 +0800

    rust study

--- study-rust/gretting/src/main.rs
+++ study-rust/gretting/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
     println!("Hello, world!");
+    // let v:Vec<i32> = Vec::new();
+    let mut v = vec![1,2,3];
+    v.push(1);
+    let third:&i32 = &v[2];
+    let third2 = v.get(0);
+    println!("{}", third);
+
+    match third2 {
+        Some(third) => println!("{}", third),
+        None => println!("no"),
+    }
 }
