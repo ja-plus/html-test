b5aa289e01b4a020dd74c8feb518428904d01c9f
JA+ <601286825@qq.com>
2022-05-18 15:36:18 +0800

    add

--- tool-test/webpack5/src/app.js
+++ tool-test/webpack5/src/app.js
@@ -1,2 +1,3 @@
 import './style/style.css';
 import './testAxios.js';
+import './jafetch/test.js';
---
+++ tool-test/webpack5/src/jafetch/test.js
@@ -0,0 +1,21 @@
+import jafetch from 'ja-fetch';
+/* test interceptor abortController*/
+import { commonCancelRequest } from 'ja-fetch/preset/interceptors';
+let ServiceAB = jafetch.create();
+ServiceAB.interceptors.use(commonCancelRequest());
+
+const fetchBtn = document.createElement('button');
+fetchBtn.textContent = 'fetch';
+document.body.append(fetchBtn);
+fetchBtn.addEventListener('click', () => {
+  console.log('发起fetch 请求... 1s后返回结果');
+  ServiceAB.get('http://localhost:8080/timeoutTestData', {
+    params: { timeout: 1000 },
+  })
+    .then(response => {
+      console.log('fetch response :>> ', response);
+    })
+    .catch(err => {
+      console.warn('fetch请求已被取消', err);
+    });
+});
