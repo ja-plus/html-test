2f81eec665c6151372524ff3021eb435b11150bc
JA+ <601286825@qq.com>
2022-08-25 21:25:59 +0800

    test antdv cascader

---
+++ tool-test/vite-vue/src/components/Antdv/Cascader.vue
@@ -0,0 +1,59 @@
+<template>
+    <h3>Cascader</h3>
+    <a-cascader :options="options" :show-search="{ filter }" placeholder="Please select" @change="onChange" />
+</template>
+<script>
+export default {
+    data() {
+        return {
+            options: [
+                {
+                    value: 'zhejiang',
+                    label: 'Zhejiang',
+                    children: [
+                        {
+                            value: 'hangzhou',
+                            label: 'Hangzhou',
+                            children: [
+                                {
+                                    value: 'xihu',
+                                    label: 'West Lake',
+                                },
+                                {
+                                    value: 'xiasha',
+                                    label: 'Xia Sha',
+                                    disabled: true,
+                                },
+                            ],
+                        },
+                    ],
+                },
+                {
+                    value: 'jiangsu',
+                    label: 'Jiangsu',
+                    children: [
+                        {
+                            value: 'nanjing',
+                            label: 'Nanjing',
+                            children: [
+                                {
+                                    value: 'zhonghuamen',
+                                    label: 'Zhong Hua men',
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+        };
+    },
+    methods: {
+        onChange(value, selectedOptions) {
+            console.log(value, selectedOptions);
+        },
+        filter(inputValue, path) {
+            return path.some(option => option.label.toLowerCase().indexOf(inputValue.toLowerCase()) > -1);
+        },
+    },
+};
+</script>
--- tool-test/vite-vue/src/components/Antdv/index.vue
+++ tool-test/vite-vue/src/components/Antdv/index.vue
@@ -3,12 +3,14 @@
     <summary><img src="https://next.antdv.com/assets/logo.1ef800a8.svg" alt="Ant design vue" srcset="" style="height:20px;"></summary>
     <Button />
     <Table />
+    <Cascader />
   </details>
 </template>
 
 <script setup>
   import Button from './Button.vue'
   import Table from './Table.vue'
+  import Cascader from './Cascader.vue'
 </script>
 
 <style>
