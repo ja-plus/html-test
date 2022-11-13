c7e668dfeec7119e4bf1d4eebf82444c1cae9208
JA+ <601286825@qq.com>
2022-11-13 15:36:18 +0800

    rust wasm

--- study-rust/hello-wasm/src/lib.rs
+++ study-rust/hello-wasm/src/lib.rs
@@ -3,25 +3,27 @@ extern crate wasm_bindgen;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern {
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    pub fn log(s: &str);
     // 告诉rust 想要调用外部定义的函数。这些函数定义在window上。
     pub fn alert(s: &str);
     // js 定义在window上的方法
-    pub fn getData(data:String);
+    pub fn getData(data: String);
 
 }
 
 //表示这个函数可以被js调用,export greet
 #[wasm_bindgen]
-pub fn greet(name: &str) -> String{
+pub fn greet(name: &str) -> String {
     alert(&format!("Hello, {}!", name));
     String::from("res data")
 }
 
 #[wasm_bindgen]
-pub fn run(){
-    for i in 0..4{
-        println!("rust println {:?}",i);
+pub fn run() {
+    for i in 0..4 {
+        println!("rust println {:?}", i);
         getData(i.to_string());
     }
 }
