4b191b9623e500c27968fd998e8fc1fbd0099379
JA+ <601286825@qq.com>
2022-11-13 15:17:24 +0800

    rust wasm 调用window上的方法

--- study-rust/hello-wasm/src/lib.rs
+++ study-rust/hello-wasm/src/lib.rs
@@ -4,8 +4,11 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern {
-    // 告诉rust 想要调用外部定义的函数
+    // 告诉rust 想要调用外部定义的函数。这些函数定义在window上。
     pub fn alert(s: &str);
+    // js 定义在window上的方法
+    pub fn getData(data:String);
+
 }
 
 //表示这个函数可以被js调用,export greet
@@ -13,4 +16,12 @@ extern {
 pub fn greet(name: &str) -> String{
     alert(&format!("Hello, {}!", name));
     String::from("res data")
+}
+
+#[wasm_bindgen]
+pub fn run(){
+    for i in 0..4{
+        println!("rust println {:?}",i);
+        getData(i.to_string());
+    }
 }
