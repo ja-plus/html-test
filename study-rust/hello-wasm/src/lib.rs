104ea5a88653d4e4a09e7e1e25289d2e46b3b1ee
JA+ <601286825@qq.com>
2022-11-19 17:26:09 +0800

    rust 求素数性能

--- study-rust/hello-wasm/src/lib.rs
+++ study-rust/hello-wasm/src/lib.rs
@@ -1,6 +1,8 @@
 extern crate wasm_bindgen;
 
+use std::str;
 use wasm_bindgen::prelude::*;
+// use std::num; // 开平方根方法
 
 #[wasm_bindgen]
 extern "C" {
@@ -12,18 +14,57 @@ extern "C" {
     pub fn getData(data: String);
 
 }
+/**window.console.log() */
+fn console_log(s: &str) {
+    log(&format!("RustLog:{}", s));
+}
 
 //表示这个函数可以被js调用,export greet
 #[wasm_bindgen]
 pub fn greet(name: &str) -> String {
-    alert(&format!("Hello, {}!", name));
+    let log_string = &format!("Hello, {}!", name);
+    // alert(&format!("Hello, {}!", name));
+    console_log(log_string);
     String::from("res data")
 }
 
 #[wasm_bindgen]
-pub fn run() {
+pub fn sub_data() {
     for i in 0..4 {
         println!("rust println {:?}", i);
         getData(i.to_string());
     }
+}
+
+/**素数，进行cpu密集任务 */
+#[wasm_bindgen]
+pub fn cpu_calc(count: i16) {
+    let mut result: Vec<i16> = vec![];
+    // 计算多少数字下
+    for i in 2..count {
+        let num: f64 = i as f64;
+        let after_sqrt = num.sqrt() as i16 + 1;
+        let mut flag = true;
+
+        for j in 2..after_sqrt {
+            let dividing = i % j;
+            if dividing == 0 {
+                flag = false;
+                break;
+            }
+        }
+
+        if flag {
+            result.push(i);
+            // console_log(&format!("{},{}", &i.to_string(), &after_sqrt.to_string()));
+        }
+    }
+
+    // TODO: Vec 转字符串
+    let mut result_str = String::from("");
+    for num in result.iter() {
+        result_str = format!("{},{}",result_str,num.to_string());
+    }
+
+    console_log(&format!("rust 素数:{}", result_str));
 }
