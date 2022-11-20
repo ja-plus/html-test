b5d8a213fa5f265ba315a739d35b96fb2356c17f
JA+ <601286825@qq.com>
2022-11-20 15:49:19 +0800

    js cpu

--- study-rust/hello-wasm/src/lib.rs
+++ study-rust/hello-wasm/src/lib.rs
@@ -38,12 +38,13 @@ pub fn sub_data() {
 
 /**素数，进行cpu密集任务 */
 #[wasm_bindgen]
-pub fn cpu_calc(count: i16) {
-    let mut result: Vec<i16> = vec![];
+pub fn cpu_calc(count: i32) {
+    // let mut result: Vec<i16> = vec![];
+    let mut result = 1;
     // 计算多少数字下
     for i in 2..count {
         let num: f64 = i as f64;
-        let after_sqrt = num.sqrt() as i16 + 1;
+        let after_sqrt = num.sqrt() as i32 + 1;
         let mut flag = true;
 
         for j in 2..after_sqrt {
@@ -55,16 +56,17 @@ pub fn cpu_calc(count: i16) {
         }
 
         if flag {
-            result.push(i);
+            // result.push(i);
+            result = i;
             // console_log(&format!("{},{}", &i.to_string(), &after_sqrt.to_string()));
         }
     }
 
     // TODO: Vec 转字符串
-    let mut result_str = String::from("");
-    for num in result.iter() {
-        result_str = format!("{},{}",result_str,num.to_string());
-    }
+    // let mut result_str = String::from("");
+    // for num in result.iter() {
+    //     result_str = format!("{},{}",result_str,num.to_string());
+    // }
 
-    console_log(&format!("rust 素数:{}", result_str));
+    console_log(&format!("rust 素数:{}", result));
 }
