80f120340ebbb97d1275905bf0793463797e4d96
JA+ <601286825@qq.com>
2022-11-28 21:23:21 +0800

    rust reqwest

--- study-rust/gretting/src/main.rs
+++ study-rust/gretting/src/main.rs
@@ -1,3 +1,10 @@
-fn main(){
+mod use_reqwest;
+
+#[tokio::main]
+async fn main() {
     println!("hello world");
+    let res = use_reqwest::main();
+    if let Ok(resp) = res.await {
+        println!("{:#?}", resp);
+    }
 }
