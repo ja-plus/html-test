a857aa9d65fcd6736fa5af966a295bd12894c174
JA+ <601286825@qq.com>
2022-12-04 20:15:07 +0800

    test rust env

--- study-rust/gretting/src/main.rs
+++ study-rust/gretting/src/main.rs
@@ -1,17 +1,21 @@
-use std::collections::HashMap;
+use std::env;
 
 mod use_reqwest;
 
 #[tokio::main]
 async fn main() {
+    // 接收命令行参数
+    let args: Vec<String> = env::args().collect();
+    println!("命令行参数：{:?}", args);// cargo run xxx
+    println!("参数:{}",&args[1]);
     println!("hello world");
-    
+
     let res = use_reqwest::main();
     if let Ok(resp) = res.await {
         println!("response:{:#?}", resp);
     }
     let post_res = use_reqwest::post_test();
     if let Ok(post_resp) = post_res.await {
-        println!("post Response: {:#?}",post_resp);
+        println!("post Response: {:#?}", post_resp);
     }
 }
