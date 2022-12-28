e67b41089ef456295fb970ca62b2ee53b9861abb
JA+ <601286825@qq.com>
2022-12-28 21:59:50 +0800

    try VueUse

--- tool-test/vite4/vue2/src/App.vue
+++ tool-test/vite4/vue2/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>Hello vue 2.7 {{ name }}</div>
+  <h1>Hello vue 2.7 {{ name }}</h1>
 </template>
 <script setup>
 const name = 'Tom';
---
+++ tool-test/vite4/vue3/src/components/VueUse.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>VueUse</h1>
+    <p>useMouse: x:{{ x }},y:{{ y }}</p>
+  </div>
+</template>
+<script setup>
+import { useLocalStorage, useMouse } from '@vueuse/core';
+useLocalStorage('myLS', { a: 1 });
+const { x, y } = useMouse();
+</script>
