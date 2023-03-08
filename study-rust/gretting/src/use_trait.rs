8429f04b1f1452e13a05946f0534576bd44107ba
JA+ <601286825@qq.com>
2023-03-08 10:41:23 +0800

    update rs trait

--- study-rust/gretting/src/use_trait.rs
+++ study-rust/gretting/src/use_trait.rs
@@ -11,7 +11,7 @@ impl Person {
     pub fn new() -> Self {
         Person {
             name: String::from("Jack"),
-            age: 12
+            age: 12,
         }
     }
 }
@@ -19,4 +19,42 @@ impl Show for Person {
     fn show(&self) {
         println!("name:{},age:{}", self.name, self.age);
     }
+}
+pub struct Animal {}
+impl Show for Animal {} // 此行去除，这个struct不能进入impl Show 的约束
+// 仅可以传入Person 结构体
+pub fn ha(s: Person) -> Person {
+    let person = Person {
+        name: String::from("aa"),
+        age: 12,
+    };
+    person.show();
+    s.show();
+    s.age;
+    person
+}
+// impl 关键字约束，struct一定需要impl了Show这个trait
+pub fn ha2(s: impl Show)-> impl Show {
+    s.show();
+    s
+}
+// 泛型约束
+pub fn ha3<T: Show>(s: T)-> T {
+    s.show();
+    s
+}
+// where 
+pub fn ha4<T>(s: T) -> T
+where
+    T: Show,
+{
+    s.show();
+    s
+}
+
+pub fn main(){
+    let animal = Animal{};
+    let animal = ha2(animal);
+    let animal = ha3(animal);
+    let animal = ha4(animal); // 归还所有权？
 }
