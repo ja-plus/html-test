6561b9e068366425e2556108b44d17ab439dde39
JA+ <601286825@qq.com>
2023-04-07 15:24:55 +0800

    update vue3-ant-design

--- tool-test/vite4/vue3-ts/src/App.vue
+++ tool-test/vite4/vue3-ts/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import ArcoDesign from './components/arco-design/index.vue';
+// import ArcoDesign from './components/arco-design/index.vue';
+import AntDesign from './components/ant-design/index.vue';
 </script>
 <template>
   <div>
-    <ArcoDesign></ArcoDesign>
+    <!-- <ArcoDesign></ArcoDesign> -->
+    <AntDesign />
   </div>
 </template>
 
--- tool-test/vite4/vue3-ts/vite.config.ts
+++ tool-test/vite4/vue3-ts/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+        globalVars: {
+          'border-radius-base': '4px',
+        },
+      },
+    },
+  },
   plugins: [vue()],
-})
+});
