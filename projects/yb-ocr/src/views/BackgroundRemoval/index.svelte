008e9c31c5c63e4e28cb9387ab26d5b200eb4850
JA+ <601286825@qq.com>
2023-07-20 22:26:52 +0800

    update yb-tools

--- projects/yb-ocr/src/views/BackgroundRemoval/index.svelte
+++ projects/yb-ocr/src/views/BackgroundRemoval/index.svelte
@@ -8,11 +8,19 @@
   let loadingInterval = 0;
   let loadingTime = 0;
 
-  function handleFileChange(e: Event) {
+  let fileInput;
+
+  async function handleFileChange(e: Event) {
     sourceFile = (e.target as any).files[0];
     URL.revokeObjectURL(sourceImage);
     sourceImage = URL.createObjectURL(sourceFile);
-    removal();
+    showLoading();
+    await removal();
+    hideLoading();
+  }
+
+  async function fileInputClick() {
+    fileInput.click();
   }
 
   function showLoading() {
@@ -21,20 +29,23 @@
       loadingTime++;
     }, 1000);
   }
+  function hideLoading() {
+    window.clearInterval(loadingInterval);
+    loadingInterval = 0;
+  }
   async function removal() {
-    targetImage = 'loading';
-    showLoading();
-    imglyRemoveBackground(sourceFile, {
+    let blob: Blob = await imglyRemoveBackground(sourceFile, {
+      // mode:'small'
       publicPath: '/background-removal/',
       progress: (key, current, total) => {
         console.log(`Downloading ${key}: (${current} of ${total}`);
       },
-    }).then((blob: Blob) => {
-      // The result is a blob encoded as PNG. It can be converted to an URL to be used as HTMLImage.src
-      const url = URL.createObjectURL(blob);
-      targetImage = url;
-      // console.log(url, 'sdf', blob);
     });
+
+    // The result is a blob encoded a1s PNG. It can be converted to an URL to be used as HTMLImage.src
+    const url = URL.createObjectURL(blob);
+    targetImage = url;
+    console.log(url, 'sdf', blob);
   }
 
   function downloadImage() {
@@ -47,15 +58,22 @@
 </script>
 
 <div class="bg-rm">
-  <div class="image--wrapper">
-    <div class="image image-source">
-      {#if sourceImage}
-        <img src={sourceImage} alt="源" />
-      {:else}
-        <input type="file" on:change={handleFileChange} />
-      {/if}
+  <div class="source-list">
+    <div class="image-input panel" on:keydown on:click={fileInputClick}>
+      <div class="image">
+        {#if sourceImage}
+          <img src={sourceImage} alt="源图片" />
+        {:else}
+          <input bind:this={fileInput} type="file" class="d-none" on:change={handleFileChange} />
+          <div class="ms-Button ms-Button--primary">
+            <span class="ms-Button-label">上传图片</span>
+          </div>
+        {/if}
+      </div>
     </div>
-    <div class="image image-target">
+  </div>
+  <div class="image-result">
+    <div class="panel">
       {#if loadingInterval}
         <h1>loading{'.'.repeat(loadingTime % 2)}</h1>
       {:else if targetImage}
@@ -67,41 +85,89 @@
 
 <style lang="less">
   .bg-rm {
+    --gap: 10px;
     background-color: rgb(250, 250, 250);
     flex: 1;
-    .image--wrapper {
+    display: flex;
+    .panel {
+      background-color: #fff;
+      box-shadow: 0 0 2px rgba(0, 0, 0, 0.12), 0 2px 4px rgba(0, 0, 0, 0.14);
+    }
+    .source-list {
+      box-sizing: content-box;
       display: flex;
-      justify-content: center;
-      gap: 10px;
-      padding: 10px;
-      flex: 1;
-      height: 100%;
-
+      flex-direction: column;
+      width: 200px;
+      padding: var(--gap);
+      overflow-y: auto;
+      overflow-y: overlay;
+    }
+    .image-input {
+      display: flex;
+      flex-direction: column;
+      cursor: pointer;
+      transition: box-shadow 0.2s ease;
+      &:hover {
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+      }
       .image {
-        height: 100%;
         display: flex;
-        background-color: #fff;
-        box-shadow: 0 0 2px rgba(0, 0, 0, 0.12), 0 2px 4px rgba(0, 0, 0, 0.14);
-        &.image-source {
-          width: 200px;
-          height: 200px;
-        }
-        &.image-target {
-          flex: 1;
-          > img {
-            cursor: pointer;
-            transition: transform 0.2s ease;
-            &:hover {
-              transform: scale(1.1);
-            }
-          }
-        }
+        justify-content: center;
+        align-items: center;
+        width: 200px;
+        height: 200px;
         > img {
           width: 100%;
           height: 100%;
           object-fit: contain;
         }
       }
+    }
+    .image-result {
+      padding: var(--gap) var(--gap) var(--gap) 0;
+      flex: 1;
+      .panel {
+        display: flex;
+        width: 100%;
+        height: 100%;
+        > img {
+          width: 100%;
+          height: 100%;
+          object-fit: contain;
+          cursor: pointer;
+          transition: box-shadow 0.2s ease;
+          &:hover {
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+          }
+        }
+      }
+    }
+
+    /* Fluent Design按钮样式 */
+    .ms-Button {
+      font-weight: 600;
+      padding: 5px 10px;
+      border-radius: 2px;
+      background: #0078d4;
+      color: #fff;
+      display: inline-flex;
+      align-items: center;
+      box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.1);
+      cursor: pointer;
+    }
+    .d-none {
+      display: none;
+    }
+    .ms-Button--primary {
+      background: #0078d4;
+    }
+
+    .ms-Button:hover {
+      background: #106ebe;
+    }
+
+    .ms-Button-label {
+      margin-right: 8px;
     }
   }
 </style>
