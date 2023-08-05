382e4e190faeef1673b1c34c4a21fdf280e26d8a
JA+ <601286825@qq.com>
2023-08-05 19:33:17 +0800

    rs 最长递增路径

--- study-rust/gretting/src/vector.rs
+++ study-rust/gretting/src/vector.rs
@@ -27,6 +27,8 @@ fn main() {
     v1.append(&mut v2);
     println!("v1:{}", v1[0]); // 通过下标访问
     println!("append:{:?}", v1);
+    // 初始值
+    vec![0; 10]; // 10个0
 
     // Iterators can be collected into vectors
     let collected_iterator: Vec<i32> = (0..10).collect();
@@ -39,25 +41,24 @@ fn main() {
     // insert
     let mut vec = Vec::new();
     vec.push(1);
-    vec.insert(0, 2);// 最前方插入
+    vec.insert(0, 2); // 最前方插入
     println!("vec:{:?}", vec);
-    
+
     // drain delete
-    let mut vec = vec![1,2,3,4];
+    let mut vec = vec![1, 2, 3, 4];
     vec.drain(0..1);
     println!("drain() vec:{:?},", vec);
 
     // for
-    let vec = vec![2,4,6,48];
-    for (i, n) in vec.iter().enumerate(){
-        println!("iter().enumerate():i:{},n:{}",i,n);
+    let vec = vec![2, 4, 6, 48];
+    for (i, n) in vec.iter().enumerate() {
+        println!("iter().enumerate():i:{},n:{}", i, n);
     }
     // sort
-    let mut vec = vec![2,5,1,5,2];
+    let mut vec = vec![2, 5, 1, 5, 2];
     vec.sort(); // vec.sort_by(|a,b| {a.cmp(b)})
-    println!("sort:{:?}",vec);
+    println!("sort:{:?}", vec);
 
     // with_capacity 预先设置最大容量
-    let mut vec:Vec<i32> = Vec::with_capacity(10);
-    
+    let mut vec: Vec<i32> = Vec::with_capacity(10);
 }
