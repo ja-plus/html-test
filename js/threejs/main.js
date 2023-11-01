ac52e82bbf9a44673cc5975be4cc727829919e06
JA+ <601286825@qq.com>
2023-11-01 20:50:25 +0800

    12231

--- js/threejs/main.js
+++ js/threejs/main.js
@@ -4,7 +4,7 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
 
-const cameraPosition = [0, 200, 200];
+const cameraPosition = [100, 100, 100];
 
 const scene = new THREE.Scene(); // 创建一个场景
 // 创建透视投影相机相机
@@ -22,32 +22,35 @@ renderer.setSize(window.innerWidth, window.innerHeight); // 通过调用 setSize
 document.body.appendChild(renderer.domElement); // 将渲染结果展示到页面上
 
 const geometry = new THREE.BoxGeometry(10, 10, 10); // 创建一个长宽高为10 的立方体
-const sphereGeometry = new THREE.SphereGeometry(10, 10, 10); // 球
-const cylinderGeometry = new THREE.CylinderGeometry(5, 10, 10);
+const sphereGeometry = new THREE.SphereGeometry(10); // 球
+const cylinderGeometry = new THREE.CylinderGeometry(10, 10, 20);
 
 // 材质 // 创建基础网格材质
-const materialBasic = new THREE.MeshBasicMaterial({
-  color: 0xffffff,
-  wireframe: true, //是否将几何体渲染为线框，默认值为false（即渲染为平面多边形）
-  // transparent: true, //开启透明
-  // opacity: 0.5, //设置透明度
-});
+// const materialBasic = new THREE.MeshBasicMaterial({
+//   color: 0xffffff,
+//   wireframe: true, //是否将几何体渲染为线框，默认值为false（即渲染为平面多边形）
+//   // transparent: true, //开启透明
+//   // opacity: 0.5, //设置透明度
+// });
 // const materialNormal = new THREE.MeshNormalMaterial({
 //   transparent: true, //开启透明
 //   opacity: 0.7,
 // }); // 创建法线网格材质
 
-const meshLambertMaterial = new THREE.MeshLambertMaterial({});
+const meshLambertMaterial = new THREE.MeshLambertMaterial({
+  color: 0x777777,
+});
 const meshMatcapMaterial = new THREE.MeshMatcapMaterial();
 
 // 创建一种网格，将材质包裹在几何体模型上
 const cube = new THREE.Mesh(geometry, meshMatcapMaterial);
+cube.position.set(30, 5, 30);
 
-const ball = new THREE.Mesh(sphereGeometry, materialBasic);
-ball.position.set(20, 0, 0);
+const ball = new THREE.Mesh(cylinderGeometry, meshLambertMaterial);
+ball.position.set(20, 5, 0);
 
-const cylinder = new THREE.Mesh(cylinderGeometry, meshLambertMaterial);
-cylinder.position.set(0, 0, 20);
+const cylinder = new THREE.Mesh(sphereGeometry, meshLambertMaterial);
+cylinder.position.set(0, 10, 20);
 
 // 创建多种网格，将材质包裹在几何体模型上
 // const cube = SceneUtils.createMultiMaterialObject(geometry, [materialBasic, materialNormal]);
@@ -58,16 +61,25 @@ scene.add(cube, ball, cylinder);
 
 // 光源
 // 创建光源 聚光灯光源
-const spotLight = new THREE.SpotLight(0xffffff);
-// 设置光源位置
-spotLight.position.set(0, 200, 0);
-// 设置光源照射的强度
-spotLight.intensity = 1;
-// 将光源添加到场景中
-scene.add(spotLight);
+// const spotLight = new THREE.SpotLight(0xffffff);
+// // 设置光源位置
+// spotLight.position.set(100, 100, 0);
+// // 设置光源照射的强度
+// spotLight.intensity = 1;
+// // 将光源添加到场景中
+// scene.add(spotLight);
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(20, 20, 20);
+scene.add(pointLight);
+
+// 平行光
+const directionalLight = new THREE.DirectionalLight(0xffffff, 4);
+directionalLight.castShadow = true;
+scene.add(directionalLight);
 
 //环境光:没有特定方向，整体改变场景的光照明暗
-const ambient = new THREE.AmbientLight(0xffffff, 0.4);
+const ambient = new THREE.AmbientLight(0xffffff, 0.1);
 scene.add(ambient);
 
 const loader = new FBXLoader();
@@ -88,6 +100,14 @@ loader.load('./1111.fbx', function (loadedModel) {
 const axes = new THREE.AxesHelper(10); // 坐标系轴长设置为
 //  把三维坐标系 添加到场景中
 scene.add(axes);
+// 光源辅助观察
+const pointLightHelper = new THREE.PointLightHelper(pointLight, 10);
+scene.add(pointLightHelper);
+// DirectionalLightHelper：可视化平行光
+const dirLightHelper = new THREE.DirectionalLightHelper(directionalLight, 5, 0xff0000);
+scene.add(dirLightHelper);
+// const spotLightHelper = new THREE.SpotLightHelper(spotLight);
+// scene.add(spotLightHelper);
 
 function render() {
   renderer.render(scene, camera); // 结合场景和相机进行渲染，即用摄像机拍下此刻的场景（最后一步）
@@ -101,17 +121,3 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.addEventListener('change', function () {
   render();
 }); //监听鼠标、键盘事件
-
-// window.addEventListener('wheel', e => {
-//   // console.log(e, 'a');
-//   if (e.deltaY > 0) {
-//     cameraPosition.forEach((v, i) => (cameraPosition[i] = v + 1));
-//   } else {
-//     cameraPosition.forEach((v, i) => {
-//       let nv = v - 1;
-//       cameraPosition[i] = nv < 0 ? 0 : nv;
-//     });
-//   }
-//   camera.position.set(...cameraPosition);
-//   renderer.render(scene, camera); // 结合场景和相机进行渲染，即用摄像机拍下此刻的场景（最后一步）
-// });
