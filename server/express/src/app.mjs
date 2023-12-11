ce62410b6ea9dcfd21eb6a3e1c5addb3fd7f54c0
JA+ <601286825@qq.com>
2023-12-11 15:29:03 +0800

    server sse

--- server/express/src/app.mjs
+++ server/express/src/app.mjs
@@ -19,15 +19,40 @@ app.set('view engine', 'ejs');
 
 app.use('/', router);
 
-const server = spdy.createServer(
-  {
-    key: readFileSync(path.join(__dirname, `../server.key`)),
-    cert: readFileSync(path.join(__dirname, `../server.crt`)),
-  },
-  app,
-);
+app.get('/serverPush', (req, res) => {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    Connection: 'keep-alive',
+  });
+  res.flushHeaders();
 
-server.listen('3000', () => {
-  const port = server.address().port;
-  console.log('服务已启动, https://localhost:' + port);
+  // 消息之间用空行
+  let i = 1;
+  setInterval(() => {
+    res.write(`event: customEvent\n`);
+    res.write(`id: ${i}\n`);
+    res.write(`retry: 30000\n`);
+    res.write('data:' + JSON.stringify({ a: i++ }) + '\n');
+    res.write('\n');
+    res.flushHeaders();
+  }, 1000);
+
+  setTimeout(() => {
+    res.end();
+  }, 60_000);
+  req.on('close', () => {
+    console.log('SSE connection closed!');
+  });
+});
+
+// const server = spdy.createServer(
+//   {
+//     key: readFileSync(path.join(__dirname, `../server.key`)),
+//     cert: readFileSync(path.join(__dirname, `../server.crt`)),
+//   },
+//   app,
+// );
+app.listen('3000', () => {
+  console.log('http 服务已启动, http://localhost:3000');
 });
