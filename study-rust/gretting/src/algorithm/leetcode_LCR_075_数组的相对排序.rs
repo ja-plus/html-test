1cd06d9a9f5000f544664d4cc1b626211ff51aab
JA+ <601286825@qq.com>
2023-12-12 20:21:27 +0800

    leetcode 排序

--- study-rust/gretting/src/algorithm/leetcode_LCR_075_数组的相对排序.rs
+++ study-rust/gretting/src/algorithm/leetcode_LCR_075_数组的相对排序.rs
@@ -23,12 +23,12 @@ impl Solution {
 fn main() { 
 
     let solution = Solution::new();
-    // let result = solution.relative_sort_array(
-    //     vec![2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19],
-    //     vec![2, 1, 4, 3, 9, 6],
-    // );
-    // println!("{result:?}");
-    // assert_eq!(result,vec![2,2,2,1,4,3,3,9,6,7,19]);
+    let result = solution.relative_sort_array(
+        vec![2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19],
+        vec![2, 1, 4, 3, 9, 6],
+    );
+    println!("{result:?}");
+    assert_eq!(result,vec![2,2,2,1,4,3,3,9,6,7,19]);
 
     let result = solution.relative_sort_array(
         vec![28,6,22,8,44,17],
